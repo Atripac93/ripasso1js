46f2b2e187223e1a3a6a27e3a6a2dd24083b7acf
Carlo Atripaldi <carloatrica@gmail.com>
2023-12-21 08:25:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -38,12 +38,12 @@ let finale = controlla(90, 62);
 console.log(finale);
 //4//
 function verificaInizioLos(nomeCitta) {
-    var nomeCittaMinuscolo = nomeCitta.toLowerCase();
-    if (nomeCittaMinuscolo.startsWith("los")) {
-        return nomeCitta;
-    } else {
-        return false;
-    }
+  var nomeCittaMinuscolo = nomeCitta.toLowerCase();
+  if (nomeCittaMinuscolo.startsWith("los")) {
+    return nomeCitta;
+  } else {
+    return false;
+  }
 }
 var citta1 = "Los Angeles";
 var citta2 = "New York";
@@ -51,5 +51,56 @@ var citta2 = "New York";
 var risultatoCitta1 = verificaInizioLos(citta1);
 var risultatoCitta2 = verificaInizioLos(citta2);
 
-console.log(risultatoCitta2); 
+console.log(risultatoCitta2);
 //5//
+const numbers = [45, 4, 9, 16, 25];
+let sum = numbers.reduce(myFunction);
+
+//document.querySelector("").innerHTML = "The sum is " + sum;
+
+function myFunction(total, value) {
+  return total + value;
+}
+//6//
+let ary = [1, 2, 3, 4, 5];
+const unoETre = function () {
+  if (ary.includes(1) || ary.includes(3)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(unoETre(ary));
+//7//
+let acuto = 90;
+let ottuso = 180;
+let retto = 90;
+let piatto = 180;
+const angolo = function (acuto, ottuso, retto, piatto) {
+  if (acuto < 90) {
+    console.log("acuto");
+  } else if (ottuso === 90 || ottuso <= 180) {
+    console.log("ottuso");
+  } else if (retto === 90) {
+    console.log("retto");
+  } else if (piatto === 180) {
+    console.log("piatto");
+  }
+};
+console.log(piatto)
+//8//
+
+
+const getFirstLetters = function (str) {
+  const words = str.trim().split(" ");
+  let firstLetters = "";
+
+  for (let i = 0; i < Math.min(words.length, 4); i++) {
+    firstLetters += words[i][0].toUpperCase();
+  }
+
+  return firstLetters;
+};
+
+console.log(getFirstLetters("Fabbrica Italiana Automobili Torino"));
+
